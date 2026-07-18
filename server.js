009aad461d812f5e27ca28aa74980f638074b4ec
turkimet3b-art <turkimet3b@gmail.com>
2026-07-18 23:52:00 +0300

    Increase JSON body limit to 10mb

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const adminRoutes = require('./routes/admin');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/courses', courseRoutes);
